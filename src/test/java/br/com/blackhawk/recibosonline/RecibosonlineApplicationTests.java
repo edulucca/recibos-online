f4050f330fad86efdadc5d28668e736690a31827
Eduardo De Lucca <eduardo.luccati@gmail.com>
2022-09-30 13:46:41 -0300

    Primeiro commit

---
+++ src/test/java/br/com/blackhawk/recibosonline/RecibosonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.blackhawk.recibosonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecibosonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
